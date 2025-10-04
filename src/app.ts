02f80d30a4de3733929a5a9ec6e4b9cf6d69f62b
harshit-kharayat23 <harshitkharayat15@gmail.com>
2025-10-04 22:14:25 +0530

    started typescript

---
+++ src/app.ts
@@ -0,0 +1,68 @@
+//interface extends
+
+interface User{
+    name:string,
+    email:string,
+    phone:number,
+    gender?:string,
+
+
+}
+interface Admin extends User{
+    admin?:boolean;
+}
+
+const obj1={
+    name:"Harshit",
+    email:"harshit@gmail.com",
+    phone:8191095166
+
+}
+
+
+function getUserData(obj:Admin):void{
+
+
+
+}
+
+getUserData(obj1)
+
+// Type Alias
+
+type value=number|string|null;
+let a:value
+
+a=null;
+a=10;
+
+type arg=string|number
+
+function abcd(obj:arg){}
+
+let name:string="harshit";
+function greet(name:string){
+    console.log("Hello "+name);
+}
+
+greet(name);
+
+
+function isLegal(age:number):boolean{
+    if(age>=18){
+        return true;
+    }
+    else {
+        return false;
+    }
+
+}
+function delayedFun(fn:()=>void){
+    setTimeout(fn,1000);
+}
+
+delayedFun(function(){
+    console.log("Hello")
+})
+
+console.log(isLegal(15));
